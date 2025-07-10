592f54c337b26391f08a305288c891063f3bcfde
Mosouwer Jamil <38379925+mosouwer@users.noreply.github.com>
2025-07-10 07:01:09 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+'HOST': os.getenv("DB_HOST", "localhost"),
+'PORT': os.getenv("DB_PORT", "3306"),
+'USER': os.getenv("DB_USER", "root"),
+'PASSWORD': os.getenv("DB_PASSWORD", "password"),
+'NAME': os.getenv("DB_NAME", "notes_db"),
+
     }
 }
 
